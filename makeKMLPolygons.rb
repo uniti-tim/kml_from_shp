9f4e8325e8d6b88d779a268f9dae6b72c2ddf4fe
uniti-tim <timothy.carambat@uniti.com>
2018-03-08 09:20:04 -0600

    add rescue

--- makeKMLPolygons.rb
+++ makeKMLPolygons.rb
@@ -1,11 +1,12 @@
 #NOTE: You CANNOT run this script without the GEOS Lib installed and Path'd correctly.
 #This setup will depend on your OS. This is easily done on Linux, on windows install osgeo4w and path manually (annoying)
+#This script will read in an ESRI shp fileset (shp,shx,dbf) and create a KML with specified attributes
 require 'rgeo/shapefile'
 require 'ruby_kml'
 
 #Specify here the relative path and name to your shapefile set. Exclude the filetype extension.
 path = "../../Shapefiles for Pricing Tool/Shapefiles for Pricing Tool/"
-file = "City_Boundaries_Sample_Area"
+file = "--"
 
 write_to_file = true
 puts (write_to_file)? "Will be writing records to file...":nil
@@ -31,31 +32,37 @@ RGeo::Shapefile::Reader.open("#{path+file}.shp",:assume_inner_follows_outer => t
     points_array = Array.new()
     centroid = record.geometry.centroid
     id_key = record.attributes.keys[0]
+    name = (record.attributes['NAME'] || record.attributes['Name'])? ( record.attributes['NAME'] || record.attributes['Name']) : record[id_key]
 
-    record.geometry.boundary.each do |line|
-      line.points.each do |point|
-        points_array.push([point.x,point.y])
+    begin
+      record.geometry.boundary.each do |line|
+        line.points.each do |point|
+          points_array.push([point.x,point.y])
+        end
       end
+
+      folder.features << KML::Placemark.new(
+        #These were my naming conventions knowing how my GIS files were set. Your milage may vary
+        :name => "#{name} #{ (!record.attributes[id_key].to_i.zero? && !record.attributes[id_key].nil? )? "(ID: #{record.attributes[id_key]})":nil }",
+        :address=> "#{record.attributes['OBJECTID']}", #used to store Id since Snippet isnt accessible
+        :style_url => '#transBluePoly',
+        :geometry => KML::MultiGeometry.new(
+          :features => [
+
+            KML::Polygon.new(
+              :altitude_mode => 'clampToGround',
+              :outer_boundary_is => KML::LinearRing.new(
+                :coordinates=> points_array,
+              )
+            ),
+            KML::Point.new(:coordinates=> {:lat=>centroid.y, :lng=>centroid.x}),
+          ]
+        )
+      )
+    rescue
+      puts "Record #{record.index} failed! Name: #{name}"
     end
 
-    folder.features << KML::Placemark.new(
-      #These were my naming conventions knowing how my GIS files were set. Your milage may vary
-      :name => "#{record.attributes['NAME'] || record.attributes['Name'] } #{ (!record.attributes[id_key].zero? && !record.attributes[id_key].nil? )? "(ID: #{record.attributes[id_key]})":nil }",
-      :address=> "#{record.attributes['OBJECTID']}", #used to store Id since Snippet isnt accessible
-      :style_url => '#transBluePoly',
-      :geometry => KML::MultiGeometry.new(
-        :features => [
-
-          KML::Polygon.new(
-            :altitude_mode => 'clampToGround',
-            :outer_boundary_is => KML::LinearRing.new(
-              :coordinates=> points_array,
-            )
-          ),
-          KML::Point.new(:coordinates=> {:lat=>centroid.y, :lng=>centroid.x}),
-        ]
-      )
-    )
   end
 end
 
